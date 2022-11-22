2da3f1c82442c00ac3e6008d20b7adbdd167d3b3
Rengel Maricela <maricela.rengel@gmail.com>
2022-11-22 00:36:44 -0300

    Websocket - Reentrega

--- src/main.js
+++ src/main.js
@@ -1,96 +1,25 @@
 const express = require ('express');
+const moment = require('moment');
+const { Server: HttpServer } = require('http');
+const { Server: IOServer } = require('socket.io');
+const Contenedor = require('./contenedor/contenedorFs');
+/* const { Socket } = require('dgram');  */
 const app = express();
 
 const PUERTO = process.env.PORT || 8080;
 const publicRoot = './public';
 
 //***** Hacemos la carpeta public visible
-app.use('/static', express.static(publicRoot));
+app.use(express.static(publicRoot));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 
-
-class Contenedor {
-    constructor(productos) {
-        this.productos = productos;
-    }
-
-    save(objeto) {
-        if (objeto.id) {
-        this.productos.push(objeto);
-        return objeto.id;
-        }
-
-        let id = 1;
-        this.productos.forEach((element, index) => {
-        if (element.id >= id) {
-            id = element.id + 1;
-        }
-        });
-        objeto.id = id;
-        this.productos.push(objeto);
-        return id;
-    }
-
-    update(producto) {
-        this.productos = this.productos.map((element) => {
-        if (element.id == producto.id) {
-            return producto;
-        }
-        return element;
-        });
-    }
-
-    getById(id) {
-        let objetoSeleccionado = null;
-        this.productos.forEach(element => {
-        if (element.id == id) {
-            objetoSeleccionado = element;
-        }
-        });
-        return objetoSeleccionado;
-    }
-
-    getAll() {
-        return this.productos;
-    }
-
-    deleteById(id) {
-        let indexSeleccionado = -1;
-        this.productos.forEach((element, index) => {
-        if (element.id == id) {
-            indexSeleccionado = index;
-        }
-        });
-        if (indexSeleccionado != -1) {
-        this.productos.splice(indexSeleccionado, 1);
-        }
-
-    }
-
-    deleteAll() {
-        this.productos = [];
-    }
-}
-
-const productos = new Contenedor([]);
-
-
-
-/* app.get('/productos', (peticion, respuesta) => {
-    const listaProductos = productos.getAll();
-    respuesta.render('lista', {
-        productos: listaProductos
-    });
-}); */
-/* 
-app.post('/', (peticion, respuesta) => {
-    const producto = peticion.body;
-    productos.save(producto);
-    respuesta.render('formulario', {});
-}); */
+const productos = new Contenedor('./src/bd/productos.txt');
+const mensajes = new Contenedor('./src/bd/mensajes.txt');
 
 app.get('/', (peticion, respuesta) => {
     respuesta.send('index.html', {root: publicRoot});
@@ -98,9 +27,33 @@ app.get('/', (peticion, respuesta) => {
 
 
 
-const servidor = app.listen(PUERTO, () => {
+const servidor = httpServer.listen(PUERTO, () => {
     console.log(`Servidor escuchando: ${servidor.address().port}`);
 });
 
 servidor.on('error', error => console.log(`Error: ${error}`));
 
+
+io.on('connection', async (socket) =>{
+    console.log('Cliente conectado');
+    
+    const listaProductos = await productos.getAll();
+    socket.emit('nueva-conexion', listaProductos);
+
+    socket.on("new-product", (data) => {
+        productos.save(data);
+        io.sockets.emit('producto', data);
+    });
+
+    const listaMensajes = await mensajes.getAll();
+    socket.emit('messages', listaMensajes);
+
+  //Evento para recibir nuevos mensajes
+    socket.on('new-message', async data => {
+    data.time = moment(new Date()).format('DD/MM/YYYY hh:mm:ss');
+    await mensajes.save(data);
+    const listaMensajes = await mensajes.getAll();
+    io.sockets.emit('messages', listaMensajes);
+    });
+
+});
